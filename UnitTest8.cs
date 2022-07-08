06fe996f898cce933b238b1cf751f9ab9f6550cf
Steven Denman <steven.denman@gmail.com>
2022-07-08 17:32:04 +0100

    Initial

---
+++ UnitTest8.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestTestContextIssues
+{
+    [TestClass]
+    public class UnitTest8
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var text = $"{this.GetType().Name} - {System.Reflection.MethodBase.GetCurrentMethod()?.Name}";
+            System.Console.WriteLine($"Std: {text}");
+            TestContext.WriteLine($"TC: {text}");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var text = $"{this.GetType().Name} - {System.Reflection.MethodBase.GetCurrentMethod()?.Name}";
+            System.Console.WriteLine($"Std: {text}");
+            TestContext.WriteLine($"TC: {text}");
+        }
+    }
+}
